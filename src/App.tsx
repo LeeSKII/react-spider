b6e53369050501ad4c3c568efc3004fcc62e1e47
DESKTOP-2CGHMPC\WLZX <leeskiikseel@outlook.com>
2024-09-08 15:47:10 +0800

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [msg, setMsg] = useState("");
+  function getSpiderData() {
+    fetch(`${import.meta.env.VITE_API_URL}/spider`).then(async (response) => {
+      const data = await response.json();
+      setMsg(data.message);
+    });
+  }
+  useEffect(() => {
+    getSpiderData();
+  }, []);
 
   return (
     <>
@@ -16,20 +24,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {msg}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
